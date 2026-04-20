0665b286e0abe0174bd252385b1785e7d6cbc428
Dell <Dell@DESKTOP-13B42NJ>
2026-04-20 09:34:47 +0530

    Connection string code added

---
+++ DapperWith4DatabaseCommunication/Data/ConnectionFactory.cs
@@ -0,0 +1,47 @@
+﻿using DapperWith4DatabaseCommunication.Interfaces;
+using Microsoft.Data.SqlClient;
+
+namespace DapperWith4DatabaseCommunication.Data
+{
+    public class ConnectionFactory : IConnectionFactory
+    {
+        /*
+iF YOU WANT READ THE CONNECTION STRING FROM APPSETTINGS.JSON FILE,
+WE HAVE ONE PREDEFINE INTEFACE IS AVAILABLE IN .NET CALLED IConfiguration, 
+WE CAN INJECT IT IN THE CONSTRUCTOR OF THE CONNECTION FACTORY CLASS AND THEN READ THE CONNECTION STRING FROM APPSETTINGS.JSON FILE.
+*/
+        private readonly IConfiguration _configuration;
+        //inject the IConfiguration interface in the constructor of the connection factory class and assign it to the private readonly field of the IConfiguration interface type.
+        public ConnectionFactory(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public SqlConnection HotelmanagementsqlConnectionString()
+        {
+            var connectionString = Convert.ToString(_configuration.GetSection("ConnectionStrings:HotelmanagementsqlConnectionString").Value);
+            SqlConnection con = new SqlConnection(connectionString);
+            return con;
+        }
+
+        public SqlConnection MidLandSqlConnectionString()
+        {
+            var connectionString = Convert.ToString(_configuration.GetSection("ConnectionStrings:MidLandSqlConnectionString").Value);
+            SqlConnection con = new SqlConnection(connectionString);
+            return con;
+        }
+
+        public SqlConnection Northwind_DBSqlConnectionString()
+        {
+            var connectionString = Convert.ToString(_configuration.GetSection("ConnectionStrings:Northwind_DBSqlConnectionString").Value);
+            SqlConnection con = new SqlConnection(connectionString);
+            return con;
+        }
+
+        public SqlConnection RestaurantDBSqlConnectionString()
+        {
+            var connectionString = Convert.ToString(_configuration.GetSection("ConnectionStrings:RestaurantDBSqlConnectionString").Value);
+            SqlConnection con = new SqlConnection(connectionString);
+            return con;
+        }
+    }
+}
---
+++ DapperWith4DatabaseCommunication/Interfaces/IConnectionFactory.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace DapperWith4DatabaseCommunication.Interfaces
+{
+    public interface IConnectionFactory
+    {
+
+        //WE NEED TO CREATE A METHOD FOR EACH DATABASE CONNECTION STRING IN THE CONNECTION FACTORY INTERFACE AND IMPLEMENT IT IN THE CONNECTION FACTORY CLASS.
+        SqlConnection MidLandSqlConnectionString();
+        SqlConnection Northwind_DBSqlConnectionString();
+        SqlConnection HotelmanagementsqlConnectionString();
+        SqlConnection RestaurantDBSqlConnectionString();
+    }
+}
