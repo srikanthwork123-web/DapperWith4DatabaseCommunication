a2e4fca8a31ea9409d059fb454d6dc8f40c6939a
Dell <Dell@DESKTOP-13B42NJ>
2026-06-02 20:38:46 +0530

    Stringvs stringbuilder Question Description

--- DapperWith4DatabaseCommunication/Utils/ValidationMessages.cs
+++ DapperWith4DatabaseCommunication/Utils/ValidationMessages.cs
@@ -63,6 +63,25 @@ namespace DapperWith4DatabaseCommunication.Utils
  * if the filed does not contain any value then we need to raise a validation messages for that one.
  * =>here some fileds are optional,those fileds you can ignore to apply the validations.
  * =>whatever the fileds are mandatory you can write validation for those fileds,if not mandatory,you can ignore those fileds for validation.
+ * 2.what is the diffrence between string and stringbuilder?
+ * String is immutable,It means once you create a string object, you cannot change its value. If you try to modify a string, it will create a new string object in memory. So if you have multiple string concatenations, it can lead to performance issues due to the creation of multiple objects.
+ * stringbuilder is mutable,It means you can modify the value of a stringbuilder object without creating a new object. It provides methods like Append() to add new strings to the existing string. This can be more efficient when you have multiple string manipulations, as it reduces the number of objects created in memory.
+ * example:
+ * ==========
+ *   //don't use multiple string  concadination purpose below process.
+            //if you use a normal string for concadination,each and every string adding/manuplulation,it will create new object.
+            //So realtime ,if any String Adding Requirments are there use(stringBuilder concept).
+            //Dont use like below process
+            string a = "hello";//it will create object
+            string b = "hai";//it will create object
+            string c = "hyderabad";//it will create object
+            string result = a + b + c;//it will create object (Here total 4 objects created)
+
+            //======================Use this string builder process for string concadination============
+            //use stringbuilder concept beacuse it reduces the no.of object creation.
+            //if you use a stringbuilder,each and every string adding/manuplulation,it will  not create a new object.
+            //StringBuilder Having Append() method ,it will add the new string to existing string.
+            // Initialize a StringBuilder to store validation messages
  */
 
 
